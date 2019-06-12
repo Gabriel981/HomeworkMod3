e96888d5a03b271e56e2181da036dfbd4f8fdb4d
Alex Fierascu <ioan-alexandru.fierascu@externals.adidas.com>
2019-06-12 16:20:35 +0300

    nouTeste - add ignore file - change the driver call

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -1,57 +1,67 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+
 public class Homework {
 
-    WebDriver driver;
+  WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gabriel\\Downloads\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.get("http://automationpractice.com/index.php");
 
-        System.out.println("The current url of the webpage: ");
-        System.out.println(driver.getCurrentUrl());
-        System.out.println("The title of the webpage: ");
-        System.out.println(driver.getTitle());
+  @BeforeMethod
+  public void setUp () {
 
-        driver.manage().window().maximize();
+    System.setProperty(
+        "webdriver.chrome.driver",
+        "C:\\proiecte\\pentastagiu\\chromedriver.exe"
+    );
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    WebDriverWait wait = new WebDriverWait(driver, 1);
+    /*driver.get("http://automationpractice.com/index.php");
 
-        WebDriverWait wait = new WebDriverWait(driver, 1);
-        //wait.until(ExpectedConditions.elementToBeClickable(By.id(id)))
-    }
+    System.out.println("The current url of the webpage: ");
+    System.out.println(driver.getCurrentUrl());
+    System.out.println("The title of the webpage: ");
+    System.out.println(driver.getTitle());
 
-    @org.testng.annotations.Test(priority=0)
-    public void test1(){
-       // To repair this tommorrow
-       Webpage_Important webImport = new Webpage_Important();
-       webImport.searchBarTesting1();
-    }
 
-    @org.testng.annotations.Test(priority=1)
-    public void test12() {
 
-        //To repair this tommorrow
-        Webpage_Important searchTesting = new Webpage_Important();
-        searchTesting.searchBarTesting2();
-    }
+    WebDriverWait wait = new WebDriverWait(driver, 1);*/
+    //wait.until(ExpectedConditions.elementToBeClickable(By.id(id)))
+  }
 
-    @org.testng.annotations.Test(priority = 2)
-    public void test2() {
-        //To repair this tommorrow
-        Webpage_elements textOnPageDestination = new Webpage_elements();
-        textOnPageDestination.CompareText();
-    }
 
-    @AfterMethod
-    public void closeUp() {
-        driver.quit();
-    }
+  @org.testng.annotations.Test(priority = 0)
+  public void test1 () {
+    // To repair this tommorrow
+    Webpage_Important webImport = new Webpage_Important();
+    webImport.searchBarTesting1();
+  }
+
+
+  @org.testng.annotations.Test(priority = 1)
+  public void test12 () {
+
+    //To repair this tommorrow
+    Webpage_Important searchTesting = new Webpage_Important();
+    searchTesting.searchBarTesting2();
+  }
+
+
+  @org.testng.annotations.Test(priority = 2)
+  public void test2 () {
+    //To repair this tommorrow
+    Webpage_elements textOnPageDestination = new Webpage_elements();
+    textOnPageDestination.CompareText();
+  }
+
+
+  @AfterMethod
+  public void closeUp () {
+
+    driver.quit();
+  }
 }
